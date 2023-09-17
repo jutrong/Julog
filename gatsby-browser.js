fad50e1f39c7e980d333f594799a450789fcc364
[SW5] 이주현 <leek71358@gmail.com>
2023-09-18 01:11:15 +0900

    ssr redux 사용

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,7 @@
 import "prismjs/themes/prism-tomorrow.css"
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
- */
-
-// You can delete this file if you're not using it
 import React from "react"
 import { Provider } from "react-redux"
-import store from "./src/modules/Store" // 실제 경로로 변경하세요.
+import store from "./src/modules/Store"
 
 export const wrapRootElement = ({ element }) => {
   return <Provider store={store}>{element}</Provider>
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,11 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
- */
+import React from "react"
+import { Provider } from "react-redux"
+import store from "./src/modules/Store"
 
-/**
- * @type {import('gatsby').GatsbySSR['onRenderBody']}
- */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+export const wrapRootElement = ({ element }) => {
+  return <Provider store={store}>{element}</Provider>
+}
+
+export const onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: `en` })
 }
