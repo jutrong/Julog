0832d716340ac7c680bd3028e9ef0304092e929c
[SW5] 이주현 <leek71358@gmail.com>
2023-10-22 18:13:29 +0900

    페이지별 이미지

--- src/pages/sonminsoo.jsx
+++ src/pages/sonminsoo.jsx
@@ -66,30 +66,58 @@ const Sonminsoo = () => {
             </SonminsooToggleBtn>
             {isPageImg ? (
               <SonminsooPageImg isPageImg={isPageImg}>
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/0278a691-13ed-493f-8bf0-11e38b7bb302/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/83544b0a-e822-48af-a144-a1566885364a/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/4a89bfbe-bd4b-426b-9d05-43b6d3297910/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/d37d8511-ad17-4566-8d79-86d53b454ba6/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/cbaed164-b033-4df0-a6ad-39f8794566ad/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/387be322-1daf-435b-9693-5ea0ae716f6e/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/19939e0f-05ce-4b5d-9b00-25cc05cefef8/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/842bbbe6-8d50-472a-8733-0910f49afbe6/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/edf019e4-8957-4d75-b197-28df07dbc1a5/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/9d2c30f6-988e-48a5-8a27-46472c0defe0/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/7485c6f2-c687-4639-bfd6-41b3a2e8ef33/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/ac5581c0-c276-4790-a4a2-0cd1f80bdc6d/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/7d3180e9-c9a7-485c-875c-5399942b343e/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/35f6c00b-0084-47a7-a357-d10d30737929/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/ba9fd151-26a8-4ae3-b88c-2cc758edc922/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/ad62aeb3-ba4e-44e0-b9eb-112d7c6ccc37/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/07b7f569-31e9-4bb8-9840-47cd23a5dbd5/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/499eb0dd-e3aa-40f4-a838-ff2920be1339/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/5cf5613d-63bf-433e-ad9f-ec1f05230e38/image.png" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/8ac9f427-baa0-4369-8c30-6feafbac5770/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/10aa7eb7-1d03-4914-83fc-f95ef5790d00/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/1883670e-4d99-4981-8e6a-fd3937ccd993/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/2add8ec7-0f72-4e2a-bf30-cbbd4207f7f2/image.PNG" />
-                <PageImg src="https://velog.velcdn.com/images/raoni/post/98ae772d-6860-47bd-a610-6d70222d8625/image.PNG" />
+                <PageImgWrap>
+                  <PageImgTitle>유저</PageImgTitle>
+                  <PageImage>
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/0278a691-13ed-493f-8bf0-11e38b7bb302/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/83544b0a-e822-48af-a144-a1566885364a/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/4a89bfbe-bd4b-426b-9d05-43b6d3297910/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/d37d8511-ad17-4566-8d79-86d53b454ba6/image.PNG" />
+                  </PageImage>
+                </PageImgWrap>
+                <PageImgWrap>
+                  <PageImgTitle>홈</PageImgTitle>
+                  <PageImage>
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/cbaed164-b033-4df0-a6ad-39f8794566ad/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/387be322-1daf-435b-9693-5ea0ae716f6e/image.PNG" />
+                  </PageImage>
+                </PageImgWrap>
+                <PageImgWrap>
+                  <PageImgTitle>피드</PageImgTitle>
+                  <PageImage>
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/19939e0f-05ce-4b5d-9b00-25cc05cefef8/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/842bbbe6-8d50-472a-8733-0910f49afbe6/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/edf019e4-8957-4d75-b197-28df07dbc1a5/image.PNG" />
+                  </PageImage>
+                </PageImgWrap>
+                <PageImgWrap>
+                  <PageImgTitle>손민수 아이템</PageImgTitle>
+                  <PageImage>
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/9d2c30f6-988e-48a5-8a27-46472c0defe0/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/7485c6f2-c687-4639-bfd6-41b3a2e8ef33/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/ac5581c0-c276-4790-a4a2-0cd1f80bdc6d/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/7d3180e9-c9a7-485c-875c-5399942b343e/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/35f6c00b-0084-47a7-a357-d10d30737929/image.PNG" />
+                  </PageImage>
+                </PageImgWrap>
+                <PageImgWrap>
+                  <PageImgTitle>채팅</PageImgTitle>
+                  <PageImage>
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/ad62aeb3-ba4e-44e0-b9eb-112d7c6ccc37/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/07b7f569-31e9-4bb8-9840-47cd23a5dbd5/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/499eb0dd-e3aa-40f4-a838-ff2920be1339/image.PNG" />
+                  </PageImage>
+                </PageImgWrap>
+                <PageImgWrap>
+                  <PageImgTitle>마이페이지</PageImgTitle>
+                  <PageImage>
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/5cf5613d-63bf-433e-ad9f-ec1f05230e38/image.png" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/8ac9f427-baa0-4369-8c30-6feafbac5770/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/10aa7eb7-1d03-4914-83fc-f95ef5790d00/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/1883670e-4d99-4981-8e6a-fd3937ccd993/image.PNG" />
+                    <PageImg src="https://velog.velcdn.com/images/raoni/post/2add8ec7-0f72-4e2a-bf30-cbbd4207f7f2/image.PNG" />
+                  </PageImage>
+                </PageImgWrap>
               </SonminsooPageImg>
             ) : null}
           </SonminsooPage>
@@ -272,6 +300,7 @@ const SonminsooPageImg = styled.div`
   opacity: ${props => (props.isPageImg ? 1 : 0)};
   transition: opacity 0.4s ease;
 `
+
 const Toggle = styled.img`
   width: 30px;
   margin-right: 10px;
@@ -314,6 +343,7 @@ const WhatDidIdoWrap = styled.div`
   box-shadow: rgba(15, 15, 15, 0.1) 0px 0px 0px 1px,
     rgba(15, 15, 15, 0.1) 0px 2px 4px;
   margin: 40px 10px;
+  width: 100%;
 `
 const WhatDidIdoIcon = styled.p`
   margin-right: 4px;
@@ -340,4 +370,13 @@ const WhatDidIdoCodeToggle = styled.p`
 const WhatDidIdoCodeImage = styled.img`
   margin-top: 40px;
   border-radius: 10px;
+`
+const PageImgWrap = styled.div``
+const PageImgTitle = styled.p`
+  font-size: 25px;
+  font-weight: 600;
+  text-align: center;
+`
+const PageImage = styled.div`
+  margin: 30px;
 `
