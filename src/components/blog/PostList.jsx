2659f6f2554cceff9eff165c2d27d02fdcee8abd
Jutrong <leek71358@gmail.com>
2024-02-24 00:36:28 +0900

    Style: blog 페이지 스타일 수정

--- src/components/blog/PostList.jsx
+++ src/components/blog/PostList.jsx
@@ -1,136 +1,25 @@
 import React, { useEffect } from "react"
 import { Link } from "gatsby"
-import styled from "styled-components"
-import device from "../../styles/device"
+import * as S from "./style"
 
-const PostLists = ({ totalCount, edges }) => {
+const PostLists = ({ postData }) => {
   return (
     <>
-      <MainTitle>Blog</MainTitle>
-      <PostCount>{totalCount} Posts</PostCount>
-      <PostList>
-        {edges.map(({ node }) => {
-          const {
-            id,
-            frontmatter: { title, date, thumbnail, summary },
-            fields: { slug },
-          } = node
-          //TODO: StaticImage 사용해보기
-          return (
-            <Post key={id} className="post">
-              <Loading className="loading" />
-              <Link to={`${slug}`}>
-                <Thumbnail data-role="thumbnail">
-                  <img src={thumbnail} alt="post thumbnail" />
-                </Thumbnail>
-                <Text>
-                  <CreateAt>{date.split("T")[0]}</CreateAt>
-                  <Title>{title}</Title>
-                </Text>
-              </Link>
-            </Post>
-          )
-        })}
-      </PostList>
+      <S.PostList>
+        <Link to={postData.node.fields.slug}>
+          <S.PostWrap>
+            <S.ThumbnailWrap>
+              <S.Thumbnail src={postData.node.frontmatter.thumbnail} />
+            </S.ThumbnailWrap>
+            <S.ContentWrap>
+              <S.Title>{postData.node.frontmatter.title}</S.Title>
+              <S.description>{postData.node.frontmatter.summary}</S.description>
+              <S.Time>{postData.node.frontmatter.date}</S.Time>
+            </S.ContentWrap>
+          </S.PostWrap>
+        </Link>
+      </S.PostList>
     </>
   )
 }
 export default PostLists
-
-const MainTitle = styled.h1`
-  width: 100%;
-  padding-bottom: 3rem;
-  margin-top: 200px;
-  font-weight: 700;
-  font-size: 4em;
-  overflow: hidden;
-
-  a {
-    position: relative;
-    display: block;
-  }
-`
-
-const PostCount = styled.h4`
-  /* margin-top: 100px; */
-  font-weight: 700;
-  font-size: 1.4rem;
-  text-align: right;
-`
-
-const PostList = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 3fr);
-  gap: 3rem;
-  padding-bottom: 10rem;
-  margin-top: 100px;
-  color: ${({ theme }) => theme.$black};
-
-  @media ${device.tablet} {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media ${device.mobileL} {
-    grid-template-columns: repeat(1, 1fr);
-    padding-bottom: 250px;
-  }
-`
-
-const Post = styled.li`
-  position: relative;
-  overflow: hidden;
-  object-fit: cover;
-  text-align: center;
-`
-
-const Loading = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: ${({ theme }) => theme.$black};
-  transform: translateY(-120%);
-  z-index: 99;
-`
-
-const Thumbnail = styled.div`
-  cursor: pointer;
-
-  img {
-    width: 300px;
-    height: 300px;
-    object-fit: cover;
-    transition: transform 0.2s;
-    border-radius: 5px;
-    box-shadow: rgba(15, 15, 15, 0.1) 0px 0px 0px 1px,
-      rgba(15, 15, 15, 0.1) 0px 2px 4px;
-    transition: 1s ease;
-  }
-  &:hover img {
-    filter: blur(3px);
-    transition: 1s ease;
-    border-radius: 50px;
-  }
-`
-
-const Text = styled.div`
-  padding: 30px 0;
-  margin-left: 10px;
-`
-
-const Title = styled.h3`
-  color: ${({ theme }) => theme.$black};
-  font-weight: bold;
-  font-size: 20px;
-  line-height: 1.6;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`
-
-const CreateAt = styled.span`
-  color: ${({ theme }) => theme.$darkGray};
-  font-weight: 500;
-  font-size: 14px;
-  opacity: 0.53;
-`
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import PostList from "../components/blog/PostList"
@@ -8,10 +9,31 @@ import Seo from "../components/seo"
 
 const Blog = ({ data }) => {
   const { totalCount, edges } = data.allMarkdownRemark
+
   return (
     <Provider store={store}>
       <Layout>
-        <PostList totalCount={totalCount} edges={edges} />
+        <MainTitle>Blog</MainTitle>
+        <PostCount>{totalCount} Posts</PostCount>
+        <Category>
+          <CategoryItem>JAVASCRIPT</CategoryItem>
+          <CategoryItem>JAVAS</CategoryItem>
+          <CategoryItem>JA</CategoryItem>
+          <CategoryItem>리액트</CategoryItem>
+          <CategoryItem>자바스크립트</CategoryItem>
+          <CategoryItem>타입스크립트</CategoryItem>
+          <CategoryItem>JAVASCRIPT</CategoryItem>
+          <CategoryItem>JASCRIPT</CategoryItem>
+          <CategoryItem>JAVASCPT</CategoryItem>
+          <CategoryItem>JAVASCRIPT</CategoryItem>
+          <CategoryItem>JAVASCRIPT</CategoryItem>
+          <CategoryItem>JAVASCRIPTS</CategoryItem>
+          <CategoryItem>JAVASCRIPT</CategoryItem>
+          <CategoryItem>JAVASCRIPT</CategoryItem>
+        </Category>
+        {edges.map(data => (
+          <PostList key={data.node.id} postData={data} />
+        ))}
       </Layout>
     </Provider>
   )
@@ -41,5 +63,48 @@ export const query = graphql`
       }
       totalCount
     }
+  }
+`
+export const MainTitle = styled.h1`
+  width: 100%;
+  padding-bottom: 3rem;
+  margin-top: 200px;
+  font-weight: 700;
+  font-size: 4em;
+  overflow: hidden;
+
+  a {
+    position: relative;
+    display: block;
+  }
+`
+
+export const PostCount = styled.h4`
+  /* margin-top: 100px; */
+  font-weight: 700;
+  font-size: 1.4rem;
+  text-align: right;
+`
+
+export const Category = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+  gap: 12px;
+  margin: 80px auto;
+  padding: 0 20px;
+  margin-bottom: 120px;
+`
+export const CategoryItem = styled.p`
+  font-size: 13px;
+  border: 1px solid #ece7e1;
+  border-radius: 10px;
+  padding: 5px 18px;
+  transition: 0.3s;
+  cursor: pointer;
+  &:hover {
+    background-color: #191919;
+    color: white;
   }
 `
